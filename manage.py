8ef42df170d7d8be07a9a3bcd4d2d7342acfa798
amath0312 <amath0312@gmail.com>
2017-05-07 19:03:21 +0800

    remove https

--- manage.py
+++ manage.py
@@ -2,4 +2,5 @@
 app = create_app('dev')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True,ssl_context='adhoc')
+    #app.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
+    app.run(host='0.0.0.0', debug=True)
